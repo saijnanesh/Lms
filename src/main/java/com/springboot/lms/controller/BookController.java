2af042686e7d7b3ab623dc6a4c4f6a75ec3c9e18
Sai Jnanesh <saijnanesh.12345@gmail.com>
2024-06-15 17:55:15 +0530

    edit book

--- src/main/java/com/springboot/lms/controller/BookController.java
+++ src/main/java/com/springboot/lms/controller/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,5 +37,12 @@ public class BookController {
 	public String saveBook(Book book) {
 		bookRepo.save(book);
 		return "redirect:/books";
+	}
+
+	@GetMapping("/edit/book/{id}")
+	public String editBook(Model model, @PathVariable("id") Long id) {
+		Book book = bookRepo.findById(id).get();
+		model.addAttribute("book", book);
+		return "edit_book";
 	}
 }
